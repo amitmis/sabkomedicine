d219f5b288e221f3fd2e960db92699a689143eb2
Amit Misra <amitmis1994@gmail.com>
2018-04-28 23:17:40 +0530

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -30,6 +30,21 @@
 
 
     <link href="ab/css/bootstrap.min.css" rel="stylesheet">
+    
+    
+    
+    
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118346599-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-118346599-1');
+</script>
+
 
 
 </head>
